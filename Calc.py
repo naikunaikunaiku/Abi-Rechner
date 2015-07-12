ae4d817e8aa90fd76125381b43eecfd10058aef5
naikunaikunaiku <naikunaikunaiku@gmail.com>
2015-07-12 16:48:02 +0200

    need to fix empty inputs

--- Calc.py
+++ Calc.py
@@ -1,3 +1,5 @@
+
+
 __author__ = 'ivo'
 
 #import math
@@ -18,12 +20,37 @@ class Calc:
 inst = Calc()
 
 def getmarks():
-    inst.deutsch = input("deutschnote")
-    inst.biologie = input("biologienote")
+
+    inst.deutsch = str(input("deutschnote"))
+    if inst.deutsch > 15:
+        inst.deutsch = 15
+    elif inst.deutsch < 0:
+        inst.deutsch = 0
+    inst.biologie = int(input("biologienote"))
+    if inst.biologie > 15:
+        inst.biologie = 15
+    elif inst.biologie < 0:
+        inst.biologie = 0
     inst.mathe = input("mathenote")
+    if inst.mathe > 15:
+        inst.mathe = 15
+    elif inst.mathe < 0:
+        inst.mathe = 0
     inst.englisch = input("englischnote")
+    if inst.englisch > 15:
+        inst.englisch = 15
+    elif inst.englisch < 0:
+        inst.englisch = 0
     inst.soziologie = input("soziologienote")
+    if inst.soziologie > 15:
+        inst.soziologie = 15
+    elif inst.soziologie < 0:
+        inst.soziologie = 0
     inst.geschichte = input("geschichtenote")
+    if inst.geschichte > 15:
+        inst.geschichte = 15
+    elif inst.geschichte < 0:
+        inst.geschichte = 0
     inst.gotmarks = True
 
 def calcandreturn():
